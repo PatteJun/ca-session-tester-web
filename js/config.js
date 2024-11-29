cfcbf37fb7cb8061367269b1ed6bb82d6ef7add5
Christoph Paterok <christoph@chptk.de>
2024-11-29 08:34:15 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 export const CONFIG = {
-    REFRESH_ENDPOINT: 'https://ca-stage-lake.carit.ch/services/token/refresh',
+    REFRESH_ENDPOINT: 'https://ca-stage-lake.carit.ch/services/cad/token/refresh',
     REFRESH_INTERVAL: 5 * 60 * 1000, // 5 minutes in milliseconds
     COOKIE_NAMES: {
         JWT: 'session_jwt',
